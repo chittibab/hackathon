7fbf7553ce898376aef8468e8cc2a9f824518b15
chittibab <63145471+chittibab@users.noreply.github.com>
2020-05-10 13:18:25 +0530

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,28 @@
+#Generic Tree Traversal 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+        
+# create nodes
+n = Node(5)
+p = Node(6)
+q = Node(7)
+r = Node(9)
+m = Node(11)
+s = Node(12)
+l = Node(14)
+t = Node(15)
+n.add_child(p)
+n.add_child(q)
+n.add_child(r)
+p.add_child(m)
+p.add_child(s)
+q.add_child(l)
+r.add_child(t)
+
+for c in n.children:
+    print (c.data)
